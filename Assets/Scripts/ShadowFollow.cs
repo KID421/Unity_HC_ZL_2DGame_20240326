dd5696e6352c55173f0d2a28410003f620c7e2aa
KID <50666207+KID421@users.noreply.github.com>
2024-05-30 21:33:22 +0800

    Update 匯入敵人

---
+++ Assets/Scripts/ShadowFollow.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 影子跟隨系統：跟隨指定物件
+    /// </summary>
+    public class ShadowFollow : MonoBehaviour
+    {
+        [SerializeField, Header("要跟隨的物件")]
+        private Transform followTarget;
+        [SerializeField, Header("影子的 Y 軸")]
+        private float shadowY;
+
+        private void Update()
+        {
+            Follow();
+        }
+
+        /// <summary>
+        /// 跟隨
+        /// </summary>
+        private void Follow()
+        {
+            // 座標 = 獲得跟隨物件的座標
+            Vector3 point = followTarget.position;
+            // 設定座標的 Y 軸 等於 影子的 Y 軸
+            point.y = shadowY;
+            // 此物件的座標 指定成 座標
+            transform.position = point;
+        }
+    }
+}
