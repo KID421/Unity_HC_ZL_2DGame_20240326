a6e67f3808e62f2d28e3d64fef125273b07bcabe
KID <50666207+KID421@users.noreply.github.com>
2024-06-13 20:38:45 +0800

    Update 添加敵人資料

---
+++ Assets/Scripts/DataEnemy.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 敵人資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "KID/Enemy")]
+    public class DataEnemy : ScriptableObject
+    {
+        [Header("移動速度：X 軸"), Range(0, 10)]
+        public float moveSpeedXAxis = 1.5f;
+        [Header("移動速度：Z 軸"), Range(0, 5)]
+        public float moveSpeedZAxis = 1.2f;
+        [Header("X 軸停止距離"), Range(0, 5)]
+        public float stopDistanceX = 1.3f;
+        [Header("Z 軸停止距離"), Range(0, 1.5f)]
+        public float stopDistanceZ = 0.8f;
+        [Header("攻擊停止距離"), Range(0, 3f)]
+        public float attackStopDistance = 2.5f;
+        [Header("攻擊前搖時間"), Range(0, 2f)]
+        public float attackBeforeTime = 0.6f;
+        [Header("攻擊時間"), Range(0, 1f)]
+        public float attackTime = 0.3f;
+        [Header("攻擊後搖時間"), Range(0, 5f)]
+        public float attackAfterTime = 2f;
+    }
+}
--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -7,8 +7,8 @@ namespace KID
     /// </summary>
     public class EnemySystem : MonoBehaviour
     {
-        [SerializeField, Header("移動速度"), Range(0, 10)]
-        private float moveSpeed = 1.5f;
+        [SerializeField, Header("敵人資料")]
+        private DataEnemy dataEnemy;
 
         private Transform player;
         private string playerName = "玩家_騎士";
@@ -16,6 +16,7 @@ namespace KID
         private float angleLeft = 180;
         private Rigidbody rig;
         private Animator ani;
+        private string parMove = "移動數值";
 
         private void Awake()
         {
@@ -30,6 +31,9 @@ namespace KID
         private void Update()
         {
             Flip();
+            TrackPlayerZ();
+            TrackPlayerX();
+            UpdateMoveAnimation();
         }
 
         /// <summary>
@@ -39,6 +43,46 @@ namespace KID
         {
             float angle = player.position.x < transform.position.x ? angleLeft : angleRight;
             transform.eulerAngles = new Vector3(0, angle, 0);
+        }
+
+        /// <summary>
+        /// 追蹤玩家的 Z 軸
+        /// </summary>
+        private void TrackPlayerZ()
+        {
+            // 計算玩家與敵人 Z 軸的距離 = 數學.絕對值(玩家Z - 敵人Z)
+            float zDistance = Mathf.Abs(player.position.z - transform.position.z);
+            // 如果 Z 軸距離 <= Z 軸停止距離 就 跳出
+            if (zDistance <= dataEnemy.stopDistanceZ) return;
+
+            // 如果 玩家 的 Z 軸 > 敵人 的 Z 軸，方向 +1 ，否則 方向 -1
+            float direction = player.position.z > transform.position.z ? +1 : -1;
+            // print($"<color=#f33>敵人 Z 軸的方向 {direction}</color>");
+
+            // 剛體的加速度 = 新三維向量(剛體加速度X，剛體加速度Y，方向 * 移動速度 Z 軸)
+            rig.velocity = new Vector3(rig.velocity.x, rig.velocity.y, direction * dataEnemy.moveSpeedZAxis);
+        }
+
+        /// <summary>
+        /// 追蹤玩家的 X 軸
+        /// </summary>
+        private void TrackPlayerX()
+        {
+            float xDistance = Mathf.Abs(player.position.x - transform.position.x);
+            if (xDistance <= dataEnemy.stopDistanceX) return;
+
+            rig.velocity = 
+                transform.right * dataEnemy.moveSpeedXAxis + 
+                Vector3.up * rig.velocity.y + 
+                Vector3.forward * rig.velocity.z;
+        }
+
+        /// <summary>
+        /// 更新移動動畫
+        /// </summary>
+        private void UpdateMoveAnimation()
+        {
+            ani.SetFloat(parMove, rig.velocity.magnitude / dataEnemy.moveSpeedXAxis);
         }
     }
 }
