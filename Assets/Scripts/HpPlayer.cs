e53afeea009c665cb79388b2757ac61666ad2253
KID <50666207+KID421@users.noreply.github.com>
2024-06-27 20:24:24 +0800

    Update 添加傷害值效果

--- Assets/Scripts/HpPlayer.cs
+++ Assets/Scripts/HpPlayer.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace KID
 {
@@ -7,6 +9,28 @@ namespace KID
     /// </summary>
     public class HpPlayer : HpSystem
     {
+        [SerializeField, Header("圖片血條")]
+        private Image imgHp;
+        [SerializeField, Header("文字血量")]
+        private TMP_Text textHp;
 
+        private void Start()
+        {
+            UpdateUI();
+        }
+
+        private void UpdateUI()
+        {
+            imgHp.fillAmount = hp / hpMax;
+            textHp.text = $"血量 {hp}/{hpMax}";
+        }
+
+        // override 覆寫：覆寫覆類別有虛擬關鍵字的成員
+        public override void Damage(float damage)
+        {
+            // 覆類別原本的內容
+            base.Damage(damage);
+            UpdateUI();
+        }
     }
 }
--- Assets/Scripts/HpSystem.cs
+++ Assets/Scripts/HpSystem.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace KID
 {
@@ -10,7 +11,9 @@ namespace KID
         [SerializeField, Header("血量資料")]
         private DataHp dataHp;
 
-        private float hp, hpMax;
+        // protected 保護：允許此類別與子類別存取
+        protected float hp, hpMax;
+        private bool isInvicible;
 
         private void Awake()
         {
@@ -19,15 +22,30 @@ namespace KID
             hpMax = hp;
         }
 
+        // virtual 虛擬：允許子類別覆寫
         /// <summary>
         /// 受傷
         /// </summary>
         /// <param name="damage">受到的傷害</param>
-        public void Damage(float damage)
+        public virtual void Damage(float damage)
         {
+            // 如果 無敵狀態 就跳出
+            if (isInvicible) return;
+
+            StartCoroutine(Invicible());
             hp -= damage;
             if (hp <= 0) Dead();
             print($"<color=#f36>{name} 血量剩下：{hp}</color>");
+        }
+
+        private IEnumerator Invicible()
+        {
+            // 進入無敵狀態
+            isInvicible = true;
+            // 等待無敵時間
+            yield return new WaitForSeconds(dataHp.invincibleTime);
+            // 恢復沒有無敵狀態
+            isInvicible = false;
         }
 
         /// <summary>
