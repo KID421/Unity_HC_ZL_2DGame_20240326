cff3adb07eed825dee214b45e111f26d9903cc86
KID <50666207+KID421@users.noreply.github.com>
2024-07-09 21:33:39 +0800

    Update 添加攻擊編號

--- Assets/Scripts/AttackSystem.cs
+++ Assets/Scripts/AttackSystem.cs
@@ -11,21 +11,44 @@ namespace KID
         private DataAttack dataAttack;
 
         private Animator ani;
+        private int attackIndex = -1;
 
         private void OnDrawGizmos()
         {
-            
+            // 迴圈執行全部的攻擊資料
+            // 抓到陣列的數量：陣列.Length
+            for (int i = 0; i < dataAttack.attacks.Length; i++)
+            {
+                // 抓到每一筆攻擊資料
+                Attack attack = dataAttack.attacks[i];
+                // 如果攻擊資料要繪製才會執行
+                if (attack.isDraw)
+                {
+                    Gizmos.color = new Color(1, 0.3f, 0.3f, 0.6f);
+                    Gizmos.DrawCube(
+                        transform.position + transform.TransformDirection(attack.attackAreaOffset),
+                        attack.attackAreaSize);
+                }
+            }
         }
 
         private void Awake()
         {
             ani = GetComponent<Animator>();
+        }
 
-            // 迴圈 for：重複執行相同程式
-            // for + Table 兩下
-            for (int i = 1; i < 11; i++)
+        private void Update()
+        {
+            Attack();
+        }
+
+        private void Attack()
+        {
+            // 如果玩家按下左鍵，就增加攻擊編號
+            if (Input.GetKeyDown(KeyCode.Mouse0))
             {
-                print(i);
+                attackIndex++;
+                print($"<color=#f33>攻擊編號：{attackIndex}</color>");
             }
         }
     }
