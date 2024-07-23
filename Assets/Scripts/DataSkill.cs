662d5a072d8e5fd358d9257f1212342dab4031f7
KID <50666207+KID421@users.noreply.github.com>
2024-07-23 21:25:46 +0800

    Update 添加技能資料

---
+++ Assets/Scripts/DataSkill.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 技能資料
+    /// </summary>
+    [CreateAssetMenu(menuName = "KID/Skill")]
+    public class DataSkill : ScriptableObject
+    {
+        [Header("技能按鍵")]
+        public KeyCode skillKey;
+        [Header("技能動畫參數")]
+        public string skillParameter;
+        [Header("技能耗能"), Range(0, 100)]
+        public float skillCost;
+        [Header("技能傷害"), Range(0, 1000)]
+        public float skillDamage;
+        [Header("技能特效")]
+        public GameObject skillEffect;
+    }
+}
