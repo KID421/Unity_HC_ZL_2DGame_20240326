21af2c56729a571fb8647a3903916caace7e140c
KID <50666207+KID421@users.noreply.github.com>
2024-08-06 19:57:19 +0800

    Update 添加技能管理器

---
+++ Assets/Scripts/FlySystem.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 飛行系統
+    /// </summary>
+    public class FlySystem : MonoBehaviour
+    {
+        public Transform transformPlayer;
+
+        [SerializeField, Header("飛行力道")]
+        private Vector3 flyPower;
+
+        private Rigidbody2D rig;
+
+        private void Start()
+        {
+            rig = GetComponent<Rigidbody2D>();
+            // 根據玩家的座標飛行
+            rig.AddForce(
+                transformPlayer.right * flyPower.x + 
+                transformPlayer.up * flyPower.y + 
+                transformPlayer.forward * flyPower.z);
+        }
+    }
+}
---
+++ Assets/Scripts/SkillManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 技能管理器
+    /// </summary>
+    public class SkillManager : MonoBehaviour
+    {
+        [SerializeField, Header("技能資料")]
+        private DataSkill[] dataSkills;
+
+        private Animator ani;
+
+        private void Awake()
+        {
+            ani = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            SkillInput();
+        }
+
+        private void SkillInput()
+        {
+            // 使用迴圈重複執行，判斷玩家有沒有按全部技能資料裡面的某一筆
+            for (int i = 0; i < dataSkills.Length; i++)
+            {
+                // 玩家是否按下技能的按鍵
+                if (Input.GetKeyDown(dataSkills[i].skillKey))
+                {
+                    // 觸發動畫
+                    ani.SetTrigger(dataSkills[i].skillParameter);
+                    // Quaternion.identity 零角度
+                    // 生成物件(要生成的物件，生成後的座標，生成後的角度)
+                    // 生成技能特效在玩家的位置上並且角度為零 並存放在 tempSkill 內
+                    GameObject tempSkill = Instantiate(dataSkills[i].skillEffect, 
+                        transform.position, Quaternion.identity);
+
+                    // 獲得技能的飛行系統並給予玩家的座標
+                    tempSkill.GetComponent<FlySystem>().transformPlayer = transform;
+                }
+            }
+        }
+    }
+}
