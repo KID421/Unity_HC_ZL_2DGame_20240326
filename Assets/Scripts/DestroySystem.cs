8c1ab7eeb6d9720c79ba90434e965d4a2d87c9e8
KID <50666207+KID421@users.noreply.github.com>
2024-08-06 20:34:15 +0800

    Update 添加魔力介面

---
+++ Assets/Scripts/DestroySystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace KID
+{
+    /// <summary>
+    /// 刪除系統
+    /// </summary>
+    public class DestroySystem : MonoBehaviour
+    {
+        [SerializeField, Header("刪除時間"), Range(0, 10)]
+        private float destroyTime = 1;
+
+        private void Awake()
+        {
+            Destroy(gameObject, destroyTime);
+        }
+    }
+}
